c1f3b23844c5c9df49f08f2112bf02a60bc1a7cb
Keidan (K. Billonneau) <kuso.fr@gmail.com>
2023-01-09 18:54:20 +0100

    #1: fix display and number management in 64-bit

---
+++ app/src/main/java/fr/ralala/bitsedit/Radix.java
@@ -0,0 +1,28 @@
+package fr.ralala.bitsedit;
+/**
+ * ******************************************************************************
+ * <p><b>Project BitsEdit</b><br/>
+ * Radix enum
+ * </p>
+ *
+ * @author Keidan
+ * <p>
+ * License: GPLv3
+ * </p>
+ * ******************************************************************************
+ */
+public enum Radix {
+
+ DEC(10),
+ HEX(16);
+
+  private final int mRadix;
+
+  Radix(int radix) {
+    mRadix = radix;
+  }
+
+  int getRadix() {
+    return mRadix;
+  }
+}
